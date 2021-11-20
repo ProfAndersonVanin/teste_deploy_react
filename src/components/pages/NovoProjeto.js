c28b82902a1c92f59ea076ec23387a03b45cadd2
Anderson <profandersonvanin@gmail.com>
2021-11-20 18:53:04 -0300

    Pagina Home e Formulario

--- src/components/pages/NovoProjeto.js
+++ src/components/pages/NovoProjeto.js
@@ -1,5 +1,16 @@
+import ProjetoForm from '../projeto/ProjetoForm'
+import styles from './NovoProjeto.module.css'
+
 function NovoProjeto(){
-    return <h1>Página Novo Projeto</h1>
+    return (
+        <div className={styles.novoprojeto_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjetoForm />
+        </div>
+        )
 }
 
 export default NovoProjeto
+
+
